8f714409acd4f63300e64260d9018991e7593db6
Kochiyama <dev@marcelokochiyama.com>
2023-10-12 00:11:36 -0400

    feat: create http protocol helper

---
+++ src/presentation/protocols/http.rs
@@ -0,0 +1,27 @@
+pub struct HttpResponse<T> {
+    status_code: i32,
+    body: Result<T, String>,
+}
+
+impl<T> HttpResponse<T> {
+    pub fn success(body: T) -> Self {
+        Self {
+            status_code: 200,
+            body: Ok(body),
+        }
+    }
+
+    pub fn bad_request(message: String) -> Self {
+        Self {
+            status_code: 400,
+            body: Err(message),
+        }
+    }
+
+    pub fn internal_error(message: String) -> Self {
+        Self {
+            status_code: 500,
+            body: Err(message),
+        }
+    }
+}
